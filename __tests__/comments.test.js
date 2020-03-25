f4485ed2d19680eb21878afd349a2cb4e6a6dfc0
Scott Harlan <seharlan@gmail.com>
2020-03-25 15:01:16 -0700

    basic crud routes created and tested

---
+++ __tests__/comments.test.js
@@ -0,0 +1,66 @@
+const { getComment, getBlogPost } = require('../db/data-helpers');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('comment routes', () => {
+  it('creates a comment', async() => {
+    const blogPost = await getBlogPost();
+
+    return request(app)
+      .post('/api/v1/comments')
+      .send({
+        blogPostId: blogPost._id,
+        handle: 'test',
+        text: 'test 1234'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          blogPostId: blogPost._id,
+          handle: 'test',
+          text: 'test 1234',
+          __v: 0
+        });
+      });
+  });
+
+  it('gets a comment by id', async() => {
+    const blogPost = await getBlogPost();
+    const comment = await getComment({ blogPostId: blogPost._id });
+
+    return request(app)
+      .get(`/api/v1/comments/${comment._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          ...comment,
+          blogPostId: blogPost
+        });
+      });
+  });
+
+  it('updates a comment by id', async() => {
+    const comment = await getComment();
+
+    return request(app)
+      .patch(`/api/v1/comments/${comment._id}`)
+      .send({ text: 'bad!' })
+      .then(res => {
+        expect(res.body).toEqual({
+          ...comment,
+          text: 'bad!'
+        });
+      });
+  });
+
+  it('deletes a comment by id', async() => {
+    const comment = await getComment();
+
+    return request(app)
+      .delete(`/api/v1/comments/${comment._id}`)
+      .then(res => {
+        expect(res.body).toEqual(comment);
+      });
+  });
+
+});
